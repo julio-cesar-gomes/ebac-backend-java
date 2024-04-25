5f3c6aa87aa5fac637263835ef89b19a7e4cbf75
Julio Gomes <103766428+julio-cesar-gomes@users.noreply.github.com>
2024-04-25 16:21:12 -0300

    exercício modulo 09

---
+++ Console.java
@@ -0,0 +1,11 @@
+public class Console {
+
+    public static void main(String[] args) {
+        int Value1 = 1;
+        System.out.println("VALOR: " + Value1);
+        Integer Value2 = Integer.valueOf(Value1);
+        System.out.println("VALOR: " + Value1);
+
+    }
+
+}
